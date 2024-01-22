da14fc761383bb604dc53d69cb1a81b2852e4338
poring86 <matheuslino86a@gmail.com>
2024-01-22 15:44:27 -0400

    Teste

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({
     status: 200,
-    message: 'hello world!!!! aaa aaaa!!!'
+    message: 'hello world!!!! aaa aaaa!! aaaa!'
   })
 })
 
